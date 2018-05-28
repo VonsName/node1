c900683858cdbaedd992e21fef9bd3ebec5e7022
fjl <fengjianglin6817@dingtalk.com>
2018-05-28 16:46:07 +0800

    修改

---
+++ main_server.js
@@ -0,0 +1,31 @@
+const ht=require('http');
+const fs=require('fs');
+const qs=require('querystring');
+const urlLib=require('url');
+const app=ht.createServer(function (req,res) {
+    /*const obj=urlLib.parse(req.url,true);
+    const get=obj.query;
+    console.log(get);*/
+    console.log(req.url);
+    var data='';
+    req.on('data',function (str1) {
+        data+=str1;
+        fs.readFile('D:/nodeStudy/node1/bbb.txt',function (err,data) {
+            if (err){
+                console.log('读取失败');
+            } else {
+                console.log(data.toString());
+            }
+        })
+    });
+
+    req.on('end',function () {
+        fs.writeFile('D:/nodeStudy/node1/bbb.txt','测试写文件 end 事件比 on事件里面的函数先执行',function (err) {
+            if (err){
+                console.log('失败');
+            }
+        });
+        console.log(qs.parse(data));
+    });
+});
+app.listen(8888);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http=require('http');
 const ulrLib=require('url');
 const queryString=require('querystring');
+const pa=require('path');
 var server=http.createServer(function (request,response) {
     //var data=queryString.parse(request.uri);
     //console.log(request.uri);
@@ -29,14 +30,18 @@ var server=http.createServer(function (request,response) {
     //post传输数据是分段传输的 所以接受的时候也要分段接受
     //相当于事件  data事件可能会发生很多次 根据数据的大小
     var i=0;
-    request.on('data',function (data) {
+
+    request.on('data',function (str1) {
         console.log(`第${i++}次收到数据`);
-        str+=data;
+        str+=str1;
     });
 
     //end  数据全部到达的事件  事件只会发生一次
     request.on('end',function () {
         console.log(str);
+        var json=queryString.parse(str);
+        console.log(json);
+        response.write(str);
     });
 });
 server.listen(8080);
